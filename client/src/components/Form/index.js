467b917af6fa318e4903dafe61b1609ec2c84dff
Kornajun Roy <yarrrjun@gmail.com>
2020-08-03 00:41:30 -0400

    mass upload, final commit

---
+++ client/src/components/Form/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+import './style.css';
+
+function Form({ q, handleInputChange, handleFormSubmit }) {
+  return (
+    <form>
+      <div className="form-group">
+        <label htmlFor="Query">
+          <strong>Book</strong>
+        </label>
+        <input
+          className="form-control"
+          id="Title"
+          type="text"
+          value={q}
+          placeholder="Ready Player One"
+          name="q"
+          onChange={handleInputChange}
+          required
+        />
+      </div>
+      <div className="pull-right">
+        <button
+          onClick={handleFormSubmit}
+          type="submit"
+          className="btn btn-lg float-right" style={{backgroundColor: "#2196f3",color:"white"}}
+        >
+          Search
+        </button>
+      </div>
+    </form>
+  );
+}
+
+export default Form;
